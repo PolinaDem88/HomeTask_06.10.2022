54d4ac144b6cc8063d22e7f1a25675d266428cf7
Polina <Polina_piter88@mail.ru>
2022-10-09 15:10:42 +0300

    1 задача

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void GetNumber (int number)
+{
+    int result = number/10%10;
+    if (number>99 && number<1000)
+        Console.WriteLine($"{number} -> {result}");
+    else 
+        Console.WriteLine("Это число не трехзначное, введите трехзначное число");
+}
+GetNumber(5000);
