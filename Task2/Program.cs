d978afa21cc04591f104502cac23f231d9854ca0
Polina <Polina_piter88@mail.ru>
2022-10-09 15:13:19 +0300

    2 задача

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+void GetNumber (int num)
+{
+    string numericAsstring = num.ToString();
+    if(numericAsstring.Length > 2)
+    Console.WriteLine($" {numericAsstring} -> третья цифра: {numericAsstring[2]}");
+    else
+    Console.WriteLine($" {numericAsstring} -> третьей цифры нет");
+}
+GetNumber(205);
